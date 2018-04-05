32e17fdc100bfc711fb31eb17a664358767bea95
ItsEternity <ItsEternityUHC@gmail.com>
2018-04-05 13:43:12 +0100

    Colourize gamemode command

--- Common/src/main/java/me/thesquadmc/commands/CreativeCommand.java
+++ Common/src/main/java/me/thesquadmc/commands/CreativeCommand.java
@@ -50,26 +50,26 @@ public final class CreativeCommand {
 
         if (args.length() == 0) {
             if (player.getGameMode() == gameMode) {
-                player.sendMessage(CC.RED + "You are already in " + gameMode.name() + "!");
+                player.sendMessage(CC.translate("&e&lGAMEMODE &6■ &7You are already in " + gameMode.name() + "!"));
             } else {
                 player.setGameMode(gameMode);
-                player.sendMessage(CC.GREEN + "Set your gamemode to " + gameMode.name() + "!");
+                player.sendMessage(CC.translate("&e&lGAMEMODE &6■ &7You set your gamemode to &e" + gameMode.name()));
             }
 
         } else {
             Player target = Bukkit.getPlayer(args.getArg(0));
 
             if (target == null) {
-                player.sendMessage(CC.RED + args.getArg(0) + " is not online!");
+                player.sendMessage(CC.translate("&e&lGAMEMODE &6■ &7" +  args.getArg(0) + " is not online!"));
                 return;
             }
 
             target.setGameMode(gameMode);
             if (target != player) {
-                target.sendMessage(CC.GREEN + player.getDisplayName() + " set your gamemode to " + gameMode.name() + "!");
+                target.sendMessage(CC.translate("&e&lGAMEMODE &6■ " +  player.getDisplayName() + " &7set your gamemode to &e" + gameMode.name()));
             }
 
-            player.sendMessage(CC.GREEN + "Set " + target.getDisplayName() + "'s gamemode to " + gameMode.name() + "!");
+            player.sendMessage(CC.translate("&e&lGAMEMODE &6■ &7Set " + target.getDisplayName() + "'s &7gamemode to &e" + gameMode.name()));
         }
     }
 }
