784f608c3ca566f5eb29bed87f24c1047fd52029
ItsEternity <ItsEternityUHC@gmail.com>
2018-04-26 03:17:37 +0100

    Move custom encoding hook to NetworkTools

--- Common/src/main/java/me/thesquadmc/networking/mongo/Mongo.java
+++ Common/src/main/java/me/thesquadmc/networking/mongo/Mongo.java
@@ -4,11 +4,33 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
-import me.thesquadmc.Main;
+import org.bson.*;
+import org.bson.codecs.EncoderContext;
+import org.bson.codecs.UuidCodec;
+import org.bson.types.Binary;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 public final class Mongo {
+
+    static {
+        BSON.addEncodingHook(UUID.class, objectToTransform -> {
+            UUID uuid = (UUID) objectToTransform;
+            BsonDocument holder = new BsonDocument();
+
+            // Use UUIDCodec to encode the UUID using binary subtype 4
+            BsonDocumentWriter writer = new BsonDocumentWriter(holder);
+            writer.writeStartDocument();
+            writer.writeName("uuid");
+            new UuidCodec(UuidRepresentation.JAVA_LEGACY).encode(writer, uuid, EncoderContext.builder().build());
+            writer.writeEndDocument();
+
+            BsonBinary bsonBinary = holder.getBinary("uuid");
+            return new Binary(bsonBinary.getType(), bsonBinary.getData());
+        });
+
+    }
 
     private MongoClient mongoClient;
     private MongoDatabase mongoDatabase;
@@ -17,6 +39,7 @@ public final class Mongo {
         MongoCredential credential = MongoCredential.createCredential(user, db, password.toCharArray());
         mongoClient = new MongoClient(new ServerAddress(host, port),
                 Arrays.asList(credential));
+
         mongoDatabase = mongoClient.getDatabase(db);
     }
 
