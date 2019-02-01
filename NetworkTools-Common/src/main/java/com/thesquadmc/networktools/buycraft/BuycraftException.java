8b04d9ee598df73327e9ff76ae9b8835f6407b67
ckamps <ckamps2020@users.noreply.github.com>
2019-02-01 11:08:13 -0800

    Delete BuycraftException.java

--- NetworkTools-Common/src/main/java/com/thesquadmc/networktools/buycraft/BuycraftException.java
+++
@@ -1,9 +0,0 @@
-package com.thesquadmc.networktools.buycraft;
-
-public class BuycraftException extends Exception {
-
-    public BuycraftException(String message) {
-        super(message);
-    }
-
-}
